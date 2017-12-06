79d39d10623d485befcae8d1bf10f15db2fc55c1
Matija <matija.sabljak@globaldizajn.hr>
2017-12-06 12:44:27 +0100

    initial commit

---
+++ Concentration/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Matija Sabljak on 06/12/2017.
+//  Copyright © 2017 Globaldizajn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
